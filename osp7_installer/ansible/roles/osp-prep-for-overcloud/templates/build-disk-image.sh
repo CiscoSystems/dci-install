12a5f09a74698aa5566215d52ba261a5353bc368
bdemers <bdemers@apache.org>
2015-09-23 20:01:55 -0400

    Set overcloud image password all the time, and allow for password ssh (both for test access only)
    Also write tempest.conf file so we can run the cert tests directly

--- osp7_installer/ansible/roles/osp-prep-for-overcloud/templates/build-disk-image.sh
+++ osp7_installer/ansible/roles/osp-prep-for-overcloud/templates/build-disk-image.sh
@@ -48,6 +48,8 @@ correct_selinux_context () {
 
 # set password for development
 virt-customize -a overcloud-full.qcow2 --root-password password:***REMOVED***
+virt-customize --selinux-relabel -a overcloud-full.qcow2 --run-command "sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config"
+virt-customize --selinux-relabel -a overcloud-full.qcow2 --run-command "sed -i 's/PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config"
 
 # now patch the image with RPMs
 for rpm_url in ${RPM_URLS[*]}
